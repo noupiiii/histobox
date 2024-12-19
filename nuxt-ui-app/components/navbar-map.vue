a15d78c151214b41099c564b3da7ecb5ff45f4f5
noupiiii <louisbruche2022@gmail.com>
2024-12-19 14:23:51 +0100

    Ajout des navbar

---
+++ nuxt-ui-app/components/navbar-map.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <UContainer class="flex justify-between items-center py-4">
+            <ul class="flex gap-3 items-end">
+                <li class="text-xl font-semibold text-primary-500">
+                    <ULink>
+                        <h1>Histobox</h1>
+                    </ULink>
+                </li>
+                <ul v-for="link in links" :key="link.name" class="flex items-center">
+                    <li>
+                        <ULink :href="link.url" target="_blank" class="flex items-center hover:text-primary-500 transition gap-2" rel="noopener noreferrer">
+                            {{ link.name }}
+                        </ULink>
+                    </li>
+                </ul>
+            </ul>
+            <ul>
+                <li><ThemeSelector /></li>
+            </ul>
+        </UContainer>
+    </div>
+</template>
+
+<script setup>
+const links = ref([
+    { name: 'placeholder', url: '/'},
+    { name: 'placeholder', url: '/'}
+]);
+</script>
---
+++ nuxt-ui-app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-white h-fulls">
+        index.vue
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
